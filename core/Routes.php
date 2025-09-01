06aab2c758c7cab358278588a24da5a11b6187b1
Bhasura Jayaweera <bhasumagic@gmail.com>
2025-09-01 08:55:04 +0530

    added the dashboard templates

--- core/Routes.php
+++ core/Routes.php
@@ -1,6 +1,11 @@
+<?php
+
 $app->router->get('/','home.html');
 $app->router->get('/home','home.html');
 $app->router->get('/login','login.html');
 $app->router->get('/register','register.html');
-$app->router->get('/dashboard','dashboard.php');
+$app->router->get('/applicant','dashboard_app.php');
+$app->router->get('/undergrad','dashboard_und.php');
+$app->router->get('/profile','dashboard_pro.php');
+$app->router->get('/moderator','dashboard_mod.php');
 $app->router->get('/layout','layouts/layout1.html');
